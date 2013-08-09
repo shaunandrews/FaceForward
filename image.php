d264979cdfa08bd09e0a1502626fc3988e80ccec
Shaun Andrews <shaun@automattic.com>
2013-08-08 21:35:09 -0400

    Simpliying the single image template.

--- image.php
+++ image.php
@@ -14,33 +14,6 @@ get_header();
 		<?php while ( have_posts() ) : the_post(); ?>
 
 			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-				<header class="entry-header">
-					<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-
-					<div class="entry-meta">
-						<?php
-							$metadata = wp_get_attachment_metadata();
-							printf( __( 'Published <span class="entry-date"><time class="entry-date" datetime="%1$s">%2$s</time></span> at <a href="%3$s" title="Link to full-size image">%4$s &times; %5$s</a> in <a href="%6$s" title="Return to %7$s" rel="gallery">%8$s</a>', 'fforward' ),
-								esc_attr( get_the_date( 'c' ) ),
-								esc_html( get_the_date() ),
-								esc_url( wp_get_attachment_url() ),
-								$metadata['width'],
-								$metadata['height'],
-								esc_url( get_permalink( $post->post_parent ) ),
-								esc_attr( strip_tags( get_the_title( $post->post_parent ) ) ),
-								get_the_title( $post->post_parent )
-							);
-
-							edit_post_link( __( 'Edit', 'fforward' ), '<span class="edit-link">', '</span>' );
-						?>
-					</div><!-- .entry-meta -->
-
-					<nav role="navigation" id="image-navigation" class="image-navigation">
-						<div class="nav-previous"><?php previous_image_link( false, __( '<span class="meta-nav">&larr;</span> Previous', 'fforward' ) ); ?></div>
-						<div class="nav-next"><?php next_image_link( false, __( 'Next <span class="meta-nav">&rarr;</span>', 'fforward' ) ); ?></div>
-					</nav><!-- #image-navigation -->
-				</header><!-- .entry-header -->
-
 				<div class="entry-content">
 					<div class="entry-attachment">
 						<div class="attachment">
@@ -53,14 +26,6 @@ get_header();
 						</div><!-- .entry-caption -->
 						<?php endif; ?>
 					</div><!-- .entry-attachment -->
-
-					<?php
-						the_content();
-						wp_link_pages( array(
-							'before' => '<div class="page-links">' . __( 'Pages:', 'fforward' ),
-							'after'  => '</div>',
-						) );
-					?>
 				</div><!-- .entry-content -->
 
 				<footer class="entry-meta">
